a755fe0b49d5dc32cf5a4892395d2b1e890cc1e0
zigzackattack <zackbabtkis@gmail.com>
2013-07-13 00:46:52 -0700

    added support for extending models, using custom models and enabling/disabling switches

--- backbone.toggleswitch.js
+++ backbone.toggleswitch.js
@@ -1,7 +1,12 @@
 var ToggleSwitch = (function() {
     var Model = Backbone.Model.extend({
         defaults: {
-            value: true
+            value: true,
+            enable: true
+        },
+        initialize: function(attributes, defaults) {
+            _.extend(this, attributes);
+            this.set(defaults);
         },
         toggle: function(attr) {
             this.set(attr, !this.get(attr));
@@ -16,33 +21,49 @@ var ToggleSwitch = (function() {
             'click': 'toggle'
         },
         initialize: function(options) {
-            this.model = new Model(this.defaults);
+            var defaults   = (options && options.defaults) ? options.defaults : {},
+                attributes = (options && options.attributes) ? options.attributes : {};
+
+            this.model = (options && options.model) ? options.model : new Model(attributes, defaults);
+            this.options = options;
+
             this.listenTo(this.model, 'change:value', this.render);
             
             // Add to element registry.
             ToggleSwitch._elements.push(this);
-            
-            this.options = options;
                         
             return this;
         },
         render: function() {
             var isOn     = this.model.get('value') ? 'active' : '',
+                enabled  = this.model.get('enable') ? '' : 'disabled',
                 defaults = ToggleSwitch.configure(),
                 theme    = (this.options && this.options.theme) ? this.options.theme : defaults.theme,
                 animated = ((this.options && this.options.animated) || defaults.animated) ? 'animate' : '';
             
             // Build element html and styles.
             this.$el.removeClass()
-                .addClass([theme, animated, isOn, this.className].join(' '))
+                .addClass([theme, animated, isOn, enabled, this.className].join(' '))
                 .html(isOn ? '<span class="value">' + defaults.onValue + '</span>' : '<span class="value">' + defaults.offValue + '</span>');
                 
+            this.model.get('enable') ? this.delegateEvents() : this.undelegateEvents();
+
             return this;
         },
         toggle: function() {
             this.model.toggle('value');
             
             return this;
+        },
+        enable: function(enable) {
+            if(typeof enable === 'undefined') {
+                this.model.set('enable', true);
+            } else {
+                this.model.set('enable', enable);
+            }
+        },
+        disable: function() {
+            this.enable(false);
         }
     });
     
